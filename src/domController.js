f77a434e5c97a8c9c659ea7886c41aa10e54480d
Chrysanthi Matsagka <131909303+cmatsagka@users.noreply.github.com>
2026-03-10 15:09:14 +0200

    Handle icon text

--- src/domController.js
+++ src/domController.js
@@ -1,7 +1,9 @@
 export function updateWeatherUI(data) {
 	const tempElement = document.querySelector('#temp');
 	const conditionElement = document.querySelector('#condition');
+	const iconElement = document.querySelector('#icon');
 
 	tempElement.textContent = `${data.temp}`;
 	conditionElement.textContent = `${data.condition}`;
+	iconElement.textContent = `${data.icon}`;
 }
