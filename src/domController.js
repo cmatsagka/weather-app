f0e49235c804131cceb0542fafbc66ebdf6f4483
Chrysanthi Matsagka <131909303+cmatsagka@users.noreply.github.com>
2026-03-10 17:55:18 +0200

    Capitalize first letter of city

--- src/domController.js
+++ src/domController.js
@@ -3,6 +3,7 @@ export function updateWeatherUI(data, city) {
 	const conditionElement = document.querySelector('#condition');
 	const iconElement = document.querySelector('#icon');
 	const cityName = document.querySelector('#cityName');
+	city = city.charAt(0).toUpperCase() + city.slice(1).toLowerCase();
 
 	cityName.textContent = city;
 	tempElement.textContent = `${data.temp}`;
