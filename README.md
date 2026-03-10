152f9831ad685e72fef1d626b85ff93d78ed7da4
Chrysanthi Matsagka <131909303+cmatsagka@users.noreply.github.com>
2026-03-10 14:39:03 +0200

    Write the api logic for fetching the api for custom city

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# vanilla-js-template
+# Weather-app
--- src/weatherAPI.js
+++ src/weatherAPI.js
@@ -1,1 +1,18 @@
-export async function getWeather(city) {}
+const apiKey = 'ATRZQUHP9DD8V55RUJYACGRT5';
+//const apiKey = api-key;
+export async function getWeather(city) {
+	const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=metric&key=${apiKey}&contentType=json`;
+
+	try {
+		const response = await fetch(url);
+
+		if (!response.ok) {
+			throw new Error(`HTTP error! status: ${response.status}`);
+		}
+
+		const fetchedData = await response.json();
+		console.log('Success: ', fetchedData.currentConditions);
+	} catch (error) {
+		console.log('The fetch failed: ', error.message);
+	}
+}
