b9d27e7b8e49296761d9e3cbd872b681add6709a
Chrysanthi Matsagka <131909303+cmatsagka@users.noreply.github.com>
2026-03-10 19:39:25 +0200

    Add second choiced for weather state names

--- src/domController.js
+++ src/domController.js
@@ -21,9 +21,12 @@ export function updateWeatherUI(data, city, unit = 'C') {
 	const body = document.querySelector('body');
 	let weatherMood = data.icon;
 	if (data.icon.includes('cloudy')) weatherMood = 'cloudy';
-	if (data.icon.includes('rain')) weatherMood = 'rain';
-	if (data.icon.includes('clear')) weatherMood = 'clear';
-	if (data.icon.includes('snow')) weatherMood = 'snow';
+	else if (data.icon.includes('rain') || data.icon.includes('shower'))
+		weatherMood = 'rain';
+	else if (data.icon.includes('clear')) weatherMood = 'clear';
+	else if (data.icon.includes('snow')) weatherMood = 'snow';
+	else if (data.icon.includes('fog') || data.icon.includes('mist'))
+		weatherMood = 'fog';
 
 	document.body.setAttribute('data-weather', weatherMood);
 }
