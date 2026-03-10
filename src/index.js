fbe94d8840e9c63932a45d8a610c0f88d77247f8
Chrysanthi Matsagka <131909303+cmatsagka@users.noreply.github.com>
2026-03-10 18:24:03 +0200

    Add toggle btn for unit toggle

--- src/index.js
+++ src/index.js
@@ -18,6 +18,7 @@ async function handleSearch(city) {
 const searchBtn = document.querySelector('#search-btn');
 const form = document.querySelector('#searchForm');
 const input = document.querySelector('#cityInput');
+const toggleBtn = document.querySelector('#unitToggle');
 
 form.addEventListener('submit', (e) => {
 	e.preventDefault();
@@ -26,4 +27,11 @@ form.addEventListener('submit', (e) => {
 	searchBtn.disabled = true;
 
 	handleSearch(cityName);
+});
+
+toggleBtn.addEventListener('click', () => {
+	currentUnit = currentUnit === 'C' ? 'F' : 'c';
+	if (currentWeatherData) {
+		updateWeatherUI(currentWeatherData, input.value, currentUnit);
+	}
 });
